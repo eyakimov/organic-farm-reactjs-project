98bdf63d67ec2c963eb995d386bb29a75bdfb736
Emil Yakimov <yakimov@engineer.bg>
2024-07-18 14:54:47 +0300

    add routes

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Organic Farm - React project
 made by Emil Yakimov
 
+This project is for the ReactJS exam
+
 # Initialize project
  * npm create vite . -- --template react
- * 
+ * npm i react-router-dom
--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
-import About from './components/About'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
-import Banner from './components/Banner'
-import Carousel from './components/Carousel'
-import Footer from './components/Footer'
-import NavBar from './components/NavBar'
-import Products from './components/Products'
-import TopBar from './components/TopBar'
+import Home from './components/home/Home'
+import Footer from './components/footer/Footer'
+import Products from './components/products/Products'
+import About from './components/about/About'
+import Contact from './components/contact/Contact'
+import Login from './components/login/Login'
+import Register from './components/register/Register'
+import Header from './components/header/Header'
 
-function App(){
+function App() {
   return (
-    <>
-      <TopBar />
-      <NavBar />
-      <Carousel />
-      <Banner />
-      <About />
-      <Products />
+    <>      
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
       <Footer />
     </>
   )
